0249df3321f7eb7f7a0d47b63793e221c9d24e63
Eric Pesch <ericpes@umich.edu>
2018-01-06 17:17:57 -0800

    Solution 7, fetch 8 (gigasecond)

--- hamming/src/main/java/Hamming.java
+++ hamming/src/main/java/Hamming.java
@@ -1,11 +1,51 @@
 class Hamming {
-
+	
+	private String left;
+	private String right;
+	
     Hamming(String leftStrand, String rightStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    	this.setLeft(leftStrand);
+        this.setRight(rightStrand);
+        
+        //Initialize if null input
+        if(this.left == null) {
+        	this.left = "";
+        }
+        if(this.right == null) {
+        	this.right = "";
+        }
+        
+        //EXCEPTION: unequal length strands
+        if(this.left.length() != this.right.length()){
+        	throw new IllegalArgumentException("leftStrand and rightStrand must be of equal length.");
+        }
     }
 
     int getHammingDistance() {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+    	int dist = 0;
+    	for(int i = 0; i < this.left.length(); i++) {
+    		if(this.left.charAt(i) != this.right.charAt(i)) {
+    			dist++;
+    		}
+    	}
+    	
+    	return dist;
     }
+
+	public String getLeft() {
+		return left;
+	}
+
+	public void setLeft(String left) {
+		this.left = left;
+	}
+
+	public String getRight() {
+		return right;
+	}
+
+	public void setRight(String right) {
+		this.right = right;
+	}
 
 }
