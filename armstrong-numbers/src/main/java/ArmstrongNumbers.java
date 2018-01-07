45e6aa226574733455e71dc226b4274dfab0d317
Eric Pesch <ericpes@umich.edu>
2018-01-06 16:14:00 -0800

    Solution 4, fetch 5 (RNA)

--- armstrong-numbers/src/main/java/ArmstrongNumbers.java
+++ armstrong-numbers/src/main/java/ArmstrongNumbers.java
@@ -1,9 +1,14 @@
 class ArmstrongNumbers {
 
 	boolean isArmstrongNumber(int numberToCheck) {
-
-		throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+		String num = Integer.toString(numberToCheck);
+		for(int i = 0; i < num.length(); i++) {
+			numberToCheck -= Math.pow(Character.getNumericValue(num.charAt(i)), num.length());
+		}
 		
+		//If is an Armstrong number, this will equal 0 as we've subtracted every exponent value
+		//in the sequence from the input number
+		return numberToCheck == 0;
 	}
 
 }
